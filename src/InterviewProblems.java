faa1d9a537daf8f743f2572a25036ac9ab65489a
Kate McKinney <kate.mckinney210@gmail.com>
2020-07-06 16:43:49 -0500

    overriding methods

--- src/InterviewProblems.java
+++ src/InterviewProblems.java
@@ -261,6 +261,38 @@ public class InterviewProblems {
 //        }
     } // Covers basics of interfaces
 
+    public static void Overriding(){
+        class Sports{
+
+            String getName(){
+                return "Generic Sports";
+            }
+
+            void getNumberOfTeamMembers(){
+                System.out.println( "Each team has n players in " + getName() );
+            }
+        }
+
+        class Soccer extends Sports{
+            @Override
+            String getName(){
+                return "Soccer Class";
+            }
+            @Override
+            void getNumberOfTeamMembers(){
+                System.out.println( "Each team has 11 players in " + getName() );
+            }
+            // put the below stuff in main to run
+//            Sports c1 = new Sports();
+//            Soccer c2 = new Soccer();
+//            System.out.println(c1.getName());
+//            c1.getNumberOfTeamMembers();
+//            System.out.println(c2.getName());
+//            c2.getNumberOfTeamMembers();
+
+        }
+    } // Covers basics of overriding methods
+
     // Main for testing:
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
